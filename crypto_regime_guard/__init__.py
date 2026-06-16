6e46a24adff87d0467fd88196138126ae5bb5145
univcorp2-ctrl <univcorp2@gmail.com>
2026-06-16 18:59:59 +0900

    Initial crypto regime guard bot with research scanner

---
+++ crypto_regime_guard/__init__.py
@@ -0,0 +1,6 @@
+"""Paper-first crypto trading research toolkit."""
+
+from crypto_regime_guard.backtest import BacktestConfig, Backtester
+from crypto_regime_guard.strategy import RegimeGuardStrategy, StrategyConfig
+
+__all__ = ["BacktestConfig", "Backtester", "RegimeGuardStrategy", "StrategyConfig"]
